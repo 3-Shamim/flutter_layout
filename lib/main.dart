9d1e8a9ced7be664a466d01755aa0536872138da
Md Shamim <mdshamim723@gmail.com>
2020-06-25 14:03:35 +0600

    page with page controller

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutterlayout/pages/my_page_view.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Page View",
+    theme: ThemeData(
+
+    ),
+    home: MyPageView(),
+  ));
+}
